afd4356539be749d9f5bbb432a3fd701cdd7a3b8
uzinsea <akffkd96@naver.com>
2020-12-07 11:35:35 +0900

    update

--- app/src/main/java/com/example/unnieapp/MainActivity.kt
+++ app/src/main/java/com/example/unnieapp/MainActivity.kt
@@ -7,5 +7,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fun loadData(): MutableList<Post> {
+            val data: MutableList<Post> = mutableListOf()
+
+            for (i in 0 until 5){
+                val head = "[인기글]"
+                val title = "게시글 입니다"
+                var post = Post(head, title)
+                data.add(post)
+            }
+            return data;
+        }
     }
 }
