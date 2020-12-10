2892c992e7ed8d8a3e12dd58a8ffcc084454d7ad
uzinsea <akffkd96@naver.com>
2020-12-10 15:35:53 +0900

    update

--- app/src/main/java/com/example/unnieapp/SchoolPost.kt
+++ app/src/main/java/com/example/unnieapp/SchoolPost.kt
@@ -1,11 +1,19 @@
 package com.example.unnieapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class SchoolPost : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_school_post)
+
+        btn_previous.setOnClickListener {
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
