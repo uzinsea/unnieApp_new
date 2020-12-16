70799fb145f3493527deeb552c976e4e3120dc47
uzinsea <akffkd96@naver.com>
2020-12-16 23:59:26 +0900

    update

--- app/src/main/java/com/example/unnieapp/ListAdapter.kt
+++ app/src/main/java/com/example/unnieapp/ListAdapter.kt
@@ -6,19 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ListView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_sb.view.*
 import kotlinx.android.synthetic.main.listview_item.view.*
 
-class ListAdapter(val context: Context, val list: ArrayList<String>) : BaseAdapter() {
+class ListAdapter(val context: Context, val list: ArrayList<String>,val list2 : ArrayList<String>) : BaseAdapter() {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view: View = LayoutInflater.from(context).inflate(R.layout.listview_item,null)
-        //database = FirebaseDatabase.getInstance().getReference().child("board")
-//어댑터만 고치면 될 거같은데 ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ
-        return view
+        val rootView: View = LayoutInflater.from(context).inflate(R.layout.listview_item,null)
+
+        val titleView: TextView = rootView.findViewById(R.id.result_title)
+        val contentsView: TextView = rootView.findViewById(R.id.result_contents)
+
+        titleView.text = list[position]
+        contentsView.text = list2[position]
+        return rootView
+
     }
 
     override fun getItemId(position: Int): Long {
